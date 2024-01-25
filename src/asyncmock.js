0e6945c55dae5d8aaa9e5fd9856abbb9f69707da
Lucpalaa <143464230+Lucpalaa@users.noreply.github.com>
2024-01-24 23:19:12 -0300

    Segunda PreEntrega

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,8 +1,8 @@
 const productos = [
     {id: "1", nombre: "Zapas1", precio: 500, img: "../img/zapas.jpg", idCat: "2", stock: 100},
     {id: "2", nombre: "Zapas2", precio: 600, img: "../img/zapas.jpg", idCat: "2", stock: 100},
-    {id: "3", nombre: "Zapas3", precio: 700, img: "../img/zapas.jpg", idCat: "3", stock: 100},
-    {id: "4", nombre: "Zapas4", precio: 800, img: "../img/zapas.jpg", idCat: "3", stock: 100}
+    {id: "3", nombre: "Conjunto1", precio: 700, img: "../img/conjunto.jpg", idCat: "3", stock: 100},
+    {id: "4", nombre: "Conjunto2", precio: 800, img: "../img/conjunto.jpg", idCat: "3", stock: 100}
 ]
 
 export const getProductos = () => {
